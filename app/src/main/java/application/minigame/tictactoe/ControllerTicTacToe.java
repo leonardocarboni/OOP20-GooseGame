c42c3b97fa56e67d439ad88e14bb7890fdf31ffb
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-03-03 23:01:36 +0100

    create a gridPane for tictactoe

--- app/src/main/java/application/minigame/tictactoe/ControllerTicTacToe.java
+++ app/src/main/java/application/minigame/tictactoe/ControllerTicTacToe.java
@@ -1,9 +1,29 @@
 package application.minigame.tictactoe;
 
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControllerTicTacToe {
 
     //faccio iniziare il giocatore umano per primo
     private boolean isPlayerOneTurn = true;
+
+    //appean creo la classe genero 9 bottoni
+    public ControllerTicTacToe(){
+        for(int i = 0; i < 9; i++){
+            Button btn = new Button();
+            btn.setPrefSize(200,200);
+            listButton.add(btn);
+        }
+    }
+
+    //lista di bottoni
+    private List<Button> listButton = new ArrayList<>();
 
     //funzione che in base al turno mi dice a chi tocca
     private boolean checkPlayerOneTurn(){
@@ -13,6 +33,27 @@ public class ControllerTicTacToe {
     //cambia il turno del player
     private void changeTurn(){
         isPlayerOneTurn = !isPlayerOneTurn;
+    }
+
+    //creo una griglia di bottoni e la ritorno
+    //viola 103i123912803180293890 principi ma per ora okay
+    public GridPane createList(){
+        GridPane root = new GridPane();
+            root.add(listButton.get(0), 0,0,1,1);
+            root.add(listButton.get(1), 1,0,1,1);
+            root.add(listButton.get(2), 2,0,1,1);
+            root.add(listButton.get(3), 0,1,1,1);
+            root.add(listButton.get(4), 1,1,1,1);
+            root.add(listButton.get(5), 2,1,1,1);
+            root.add(listButton.get(6), 0,2,1,1);
+            root.add(listButton.get(7), 1,2,1,1);
+            root.add(listButton.get(8), 2,2,1,1);
+        return  root;
+    }
+
+    //ritorno la lista di bottoni
+    public List<Button> getList(){
+        return listButton;
     }
 
 
--- app/src/main/java/application/minigame/tictactoe/TicTacToe.java
+++ app/src/main/java/application/minigame/tictactoe/TicTacToe.java
@@ -1,21 +1,21 @@
 package application.minigame.tictactoe;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+
+import java.util.List;
 
 public class TicTacToe extends Application {
 
     //variabili per la grandezza della schermata
     private static final int SCENE_WIDTH = 600;
     private static final int SCENE_HEIGHT = 480;
+
 
     //creo un istanza del controller
     ControllerTicTacToe controller = new ControllerTicTacToe();
@@ -24,12 +24,11 @@ public class TicTacToe extends Application {
     //avvio della finestra del programma
     @Override
     public void start(Stage primaryStage) throws Exception {
-        StackPane root = new StackPane();
-
 
         /* Stage configuration */
         primaryStage.setTitle("TicTacToe");
-        primaryStage.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT));
+        //uso un contoller per creare la griglia
+        primaryStage.setScene(new Scene(controller.createList(), SCENE_WIDTH, SCENE_HEIGHT));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
