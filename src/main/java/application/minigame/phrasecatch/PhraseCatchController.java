323d787b0be67c1dc8208aa5403cf5f6256c96bd
Leonardo Carboni <carboapple@gmail.com>
2021-03-08 23:19:42 +0100

    Add sentences file and partial implementation

--- src/main/java/application/minigame/phrasecatch/PhraseCatchController.java
+++ src/main/java/application/minigame/phrasecatch/PhraseCatchController.java
@@ -1,5 +1,6 @@
 package application.minigame.phrasecatch;
 
+import application.utilities.Countdown;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -18,9 +19,46 @@ public class PhraseCatchController implements Initializable {
     @FXML
     Label textLabel, timeLabel;
 
+    private Countdown countdown;
+    private String textToCopy;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        countdown = new Countdown(15, timeLabel);
+        countdown.start();
+        initializeText();
+        initializePhrase();
+        initializeEventHandlers();
     }
-    
+
+    private void initializeText() {
+        textToCopy = "Sassi Matti cani";
+        textLabel.setText(textToCopy);
+    }
+
+    private void initializeEventHandlers() {
+        submitButton.setOnMouseClicked(e -> {
+            int secondsLeft = countdown.getSecondsLeft();
+            countdown.shutdown();
+            String textRead = inputTextField.getText();
+            int errors = checkText(textRead);
+            System.out.println("Errori: " + errors + ", Secondi Rimasti: " + secondsLeft);
+        });
+    }
+
+    private int checkText(String textRead) {
+        int errors = Math.abs(textToCopy.length() - textRead.length());
+
+        for(int i = 0; i < Math.min(textToCopy.length(), textRead.length())-1; i++) {
+            if(textRead.charAt(i) != textToCopy.charAt(i)) {
+                errors++;
+            }
+        }
+        return errors;
+    }
+
+    private void initializePhrase() {
+        
+    }
+
 }
