af3a672c9c8de2c52ac4ce5d39d9655a6c4941b1
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-01 10:17:24 +0200

    enemy and other class use Point2D for space

--- src/main/java/application/minigame/spaceshooter/Enemy.java
+++ src/main/java/application/minigame/spaceshooter/Enemy.java
@@ -1,23 +1,25 @@
 package application.minigame.spaceshooter;
 
+import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
 
 public class Enemy extends Player{
 
 
     private boolean destroyed;
+    public Point2D position_enemy;
     private int shot_received = 0;
     private int speed = Info.score+5;
 
     public Enemy(int posX, int posY, int size, Image image_enemy) {
         super(posX,posY,size,image_enemy);
-
+        position_enemy = new Point2D(posX, posY);
     }
 
     public void update(){
         super.update();
         if(!destroyed){
-            posY += speed;
+            position_enemy = new Point2D(position_enemy.getX(), position_enemy.getY()+10);
         } else{
             this.destroyed = true;
         }
