0b75a076b521538bbd2240c3f232a72890ef6b0c
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-02 11:06:58 +0200

    improved ereditation from superclass and destroyed detection

--- src/main/java/application/minigame/spaceshooter/Enemy.java
+++ src/main/java/application/minigame/spaceshooter/Enemy.java
@@ -6,22 +6,21 @@ import javafx.scene.image.Image;
 public class Enemy extends Player{
 
 
-    public boolean destroyed;
-    public Point2D position_enemy;
-    private int shot_received = 0;
     private int speed = Info.score+5;
+
 
     public Enemy(int posX, int posY, int size, Image image_enemy) {
         super(posX,posY,size,image_enemy);
-        position_enemy = new Point2D(posX, posY);
     }
+
 
     public void update(){
         super.update();
-        if(!destroyed){
-            position_enemy = new Point2D(position_enemy.getX(), position_enemy.getY()+10);
-        } else{
-            this.destroyed = true;
+        if(!destroyed && !exploding){
+            position_player = new Point2D(position_player.getX(), position_player.getY()+speed);
+        }
+        if(this.position_player.getY()>Info.HEIGHT){
+            destroyed = true;
         }
     }
 
