81f3edbad553bccfb9d879f1865702922e5de448
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-02 09:48:49 +0200

    better class for graphicsContext

--- src/main/java/application/minigame/spaceshooter/GettersGraphics.java
+++ src/main/java/application/minigame/spaceshooter/GettersGraphics.java
@@ -1,17 +1,28 @@
 package application.minigame.spaceshooter;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
 public class GettersGraphics {
 
+    private Canvas canvas = new Canvas(Info.WIDTH, Info.HEIGHT);
     private GraphicsContext gc;
 
+
     public GettersGraphics(){
-        this.gc = SpaceShooter.gc;
+        this.gc = canvas.getGraphicsContext2D();
+    }
+
+    public Canvas getCanvas(){
+        return this.canvas;
     }
 
     public GraphicsContext getGraphic(){
         return this.gc;
+    }
+
+    public void setGraphic(GraphicsContext gc){
+        this.gc = gc;
     }
 
 }
