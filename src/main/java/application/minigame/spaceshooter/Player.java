baff096c5c1c12219a4e302e9b85fa4d8901b2b5
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-02 11:08:18 +0200

    fixed a bug for image, improved distance function

--- src/main/java/application/minigame/spaceshooter/Player.java
+++ src/main/java/application/minigame/spaceshooter/Player.java
@@ -9,29 +9,30 @@ public class Player {
     public Point2D position_player;
     public final int size;
     public boolean destroyed;
+    public boolean exploding;
     public int shot_received = 0;
     private int steps_img = 0;
-    private Image player;
+    private Image image;
     private GraphicsContext gc;
 
     public Player(final int posX,final  int posY,final  int size, Image player) {
         position_player = new Point2D(posX, posY);
         this.size = size;
-        this.player = player;
+        this.image = player;
         this.gc = SpaceShooter.gc;
     }
 
     public void update(){
 
-        if(exploding()){
+        if(exploding){
             this.steps_img++;
         }
         destroyed = this.steps_img > Info.EXPLOSION_IMG_NUM;
     }
 
     public void draw(){
-        if(!exploding()) {
-            gc.drawImage(Info.PLAYER_IMG,position_player.getX(), position_player.getY(), this.size, this.size);
+        if(!exploding) {
+            gc.drawImage(this.image,position_player.getX(), position_player.getY(), this.size, this.size);
         } else{
             gc.drawImage(Info.EXPLOSION_IMG, Info.EXPLOSION_IMG_NUM % Info.EXPLOSION_COL * Info.EXPLOSION_WIDTH,
                     (Info.EXPLOSION_IMG_NUM / Info.EXPLOSION_ROWS) * Info.EXPLOSION_HEIGHT + 1,
@@ -40,17 +41,20 @@ public class Player {
         }
     }
 
-    public boolean exploding(){
-        return shot_received == Info.SHOTS_TO_PLAYER;
-    }
-
     public Shot shot(){
         return new Shot((int)position_player.getX() + 5, (int)position_player.getY() + 5, 10);
     }
 
     public boolean touch(final Enemy enemy){
-        int distance_enemy_player = (int) position_player.distance(enemy.position_enemy);
-        return distance_enemy_player < enemy.size / 2 + this.size / 2;
+        double distance_enemy_player = Info.distance(this.position_player.getX() + size / 3, this.position_player.getY() + size ,
+                enemy.position_player.getX() + enemy.size / 3, enemy.position_player.getY() + enemy.size );
+        return distance_enemy_player < enemy.size / 3 + this.size / 3;
+    }
+
+
+    public void explode(){
+        this.exploding = true;
+        this.steps_img = -1;
     }
 
 }
