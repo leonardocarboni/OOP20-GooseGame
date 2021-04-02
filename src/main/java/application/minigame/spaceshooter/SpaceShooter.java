1655becc46cf40b15b37fe3f45100c789bb6e0da
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-02 09:53:20 +0200

    fixed a bug of moving, and now draw the playerImage

--- src/main/java/application/minigame/spaceshooter/SpaceShooter.java
+++ src/main/java/application/minigame/spaceshooter/SpaceShooter.java
@@ -4,11 +4,15 @@ package application.minigame.spaceshooter;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -25,19 +29,24 @@ public class SpaceShooter extends Application {
     private Player player;
     private double mouseX;
     public static GraphicsContext gc;
-
+    private Canvas canvas = new GettersGraphics().getCanvas();
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(Info.WIDTH, Info.HEIGHT);
         gc = canvas.getGraphicsContext2D();
         Timeline animation = new Timeline(new KeyFrame(Duration.millis(60), e -> run(gc)));
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.play();
-
         canvas.setCursor(Cursor.MOVE);
         canvas.setOnMouseMoved(e -> mouseX = e.getX());
+        canvas.setOnMouseClicked(e -> {
+            if(shots.size() < 2) shots.add(player.shot());
+            if(Info.isOver) {
+                Info.isOver = false;
+                initialize();
+            }
+        });
 
         initialize();
         primaryStage.setTitle("SpaceShooter");
@@ -47,12 +56,34 @@ public class SpaceShooter extends Application {
 
     private void initialize(){
         IntStream.range(0,10).forEach(i -> enemies.add(new Enemy(rnd.nextInt(450),500,Info.SIZE_P,Info.ENEMY_IMG)));
-        player = new Player(200,100,Info.SIZE_P, Info.PLAYER_IMG);
+        player = new Player(Info.WIDTH/2,Info.HEIGHT-Info.SIZE_P,Info.SIZE_P, Info.PLAYER_IMG);
         shots = new ArrayList<>();
         Info.score = 0;
     }
 
     private void run(GraphicsContext gc) {
-        gc.fillRect(0,0,Info.WIDTH, Info.HEIGHT);
+        gc.setFill(Color.grayRgb(20));
+        gc.fillRect(0, 0, Info.WIDTH, Info.HEIGHT);
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setFont(Font.font(20));
+        gc.setFill(Color.WHITE);
+        gc.fillText("Score: " + Info.score, 60,  20);
+
+        if(Info.isOver){
+            gc.setFill(Color.RED);
+            gc.fillText("You lost, score: " + Info.score, Info.WIDTH, Info.HEIGHT);
+        }
+
+        player.update();
+        player.draw();
+        player.position_player = new Point2D(mouseX,player.position_player.getY() );
+
+
+
+
+
+
+        Info.isOver = player.destroyed;
+
     }
 }
