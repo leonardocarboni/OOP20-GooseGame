289e0dd0d5c32cf7c48e27e6de902ac793fb71f8
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-02 11:09:27 +0200

    improved collision and speed based on score

--- src/main/java/application/minigame/spaceshooter/Shot.java
+++ src/main/java/application/minigame/spaceshooter/Shot.java
@@ -7,7 +7,7 @@ public class Shot {
 
     public Point2D position_shot;
     private final int size;
-    private int speed;
+    private int speed = Info.score+10;
     public boolean noShot;
     private GraphicsContext gc;
 
@@ -18,7 +18,7 @@ public class Shot {
     }
 
     public void update(){
-        position_shot = new Point2D(position_shot.getX(), position_shot.getY()+speed);
+        position_shot = new Point2D(position_shot.getX(), position_shot.getY()-speed);
     }
 
     public void draw(){
@@ -26,7 +26,8 @@ public class Shot {
     }
 
     public boolean collide(final Enemy enemy){
-        int distance_enemy_shot = (int) position_shot.distance(enemy.position_enemy);
+        double distance_enemy_shot = Info.distance(this.position_shot.getX() + size / 3, this.position_shot.getY() + size ,
+                enemy.position_player.getX() + enemy.size / 3, enemy.position_player.getY() + enemy.size );
         return distance_enemy_shot < enemy.size / 2 + this.size / 2;
     }
 
