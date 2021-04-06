8d18b15d0b58712bcbda2d34d3a2c1d6d0b1f062
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-06 12:28:28 +0200

    improved speed system

--- src/main/java/application/minigame/spaceshooter/Enemy.java
+++ src/main/java/application/minigame/spaceshooter/Enemy.java
@@ -6,7 +6,7 @@ import javafx.scene.image.Image;
 public class Enemy extends Player{
 
 
-    private int speed = Info.score+5;
+    //private int speed = Info.score+1;
 
 
     public Enemy(int posX, int posY, int size, Image image_enemy) {
@@ -17,11 +17,15 @@ public class Enemy extends Player{
     public void update(){
         super.update();
         if(!destroyed && !exploding){
-            position_player = new Point2D(position_player.getX(), position_player.getY()+speed);
+            position_player = new Point2D(position_player.getX(), position_player.getY()+calcSpeed());
         }
         if(this.position_player.getY()>Info.HEIGHT){
             destroyed = true;
         }
+    }
+
+    private int calcSpeed(){
+        return (int) (Info.score +2) / 2;
     }
 
 }
