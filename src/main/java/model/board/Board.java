4a7ee998ec2b8882216436e04be1203c72537045
Andrea Ongaro <andrea.ongaro2@studio.unibo.it>
2021-04-08 21:07:49 +0200

    Update method's comments

--- src/main/java/model/board/Board.java
+++ src/main/java/model/board/Board.java
@@ -3,9 +3,22 @@ package model.board;
 import java.util.List;
 
 import model.box.Box;
+import model.player.PlayerImpl;
 
 public interface Board {
 
+	/*
+	 * Function to create the board of game
+	 * @param size of game
+	 * @param number of minigames in the board
+	 * @return List of boxes that equals to the game board
+	 */
 	List<Box> generateBoard();
 
+	/*
+	 * Function to get type of box
+	 * @param a player
+	 * @return type of box where the player is above now 
+	 */
+	Box getBox(final PlayerImpl p);
 }
