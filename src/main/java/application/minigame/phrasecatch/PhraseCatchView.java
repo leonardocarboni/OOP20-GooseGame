176ffea8e4cfbe9fb7d13653866aa3c7624abfd7
Leonardo Carboni <carboapple@gmail.com>
2021-04-11 11:31:03 +0200

    Update phrasecatch and cableconnect mg to extend View and switch to showAndWait

--- src/main/java/application/minigame/phrasecatch/PhraseCatchView.java
+++ src/main/java/application/minigame/phrasecatch/PhraseCatchView.java
@@ -3,18 +3,13 @@ package application.minigame.phrasecatch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+import view.View;
+import view.ViewType;
 
-import java.io.IOException;
-
-public class PhraseCatchView{
+public class PhraseCatchView extends View {
 
     @FXML
     Button submitButton;
@@ -23,24 +18,8 @@ public class PhraseCatchView{
     @FXML
     Label textLabel, timeLabel;
 
-    private final Stage primaryStage = new Stage();
-    private static final String LAYOUT_LOCATION = "layouts/phrasecatch.fxml";
-    private static final String LOGO_LOCATION = "logo.png";
-
     public PhraseCatchView(){
-        try {
-            final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource(LAYOUT_LOCATION));
-            loader.setController(this);
-            final Scene scene = new Scene(loader.load());
-            primaryStage.initModality(Modality.APPLICATION_MODAL);
-            primaryStage.setTitle("[GooseGame] Phrase Catch");
-            primaryStage.getIcons().add(new Image(LOGO_LOCATION));
-            primaryStage.setOnHiding(e -> primaryStage.setIconified(true));
-            primaryStage.setScene(scene);
-            primaryStage.setResizable(false);
-        }catch(IOException e) {
-            e.printStackTrace();
-        }
+        super.createStage(ViewType.PHRASE_CATCH);
     }
 
     /**
@@ -57,10 +36,6 @@ public class PhraseCatchView{
 
     public String getInputText(){
         return inputTextField.getText();
-    }
-
-    public void show() {
-        primaryStage.showAndWait();
     }
 
     public void addButtonListener(final EventHandler<ActionEvent> eventHandler) {
