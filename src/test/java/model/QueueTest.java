fffadcf7636dec5991eefc61207ea9dde4b48765
Andrea Ongaro <andrea.ongaro2@studio.unibo.it>
2021-04-12 22:11:57 +0200

    Add order player test

--- src/test/java/model/QueueTest.java
+++ src/test/java/model/QueueTest.java
@@ -2,24 +2,29 @@ package model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import application.minigame.phrasecatch.PhraseImpl;
 import model.player.PlayerColor;
 import model.player.PlayerImpl;
 import model.queue.QueueImpl;
 
 class QueueTest {
 
+	private QueueImpl ql;
+
 	@Test
 	void iterate() {
+		ql = new QueueImpl();
 		final List<PlayerImpl> list = new ArrayList<>();
 		list.add(new PlayerImpl("Ciao", PlayerColor.PINK));
 		list.add(new PlayerImpl("Ciao2", PlayerColor.PINK));
 		list.add(new PlayerImpl("Ciao4", PlayerColor.PINK));
-		
-		final QueueImpl ql = new QueueImpl();
 		ql.setStartingQueue(list);
 		ql.resetIterator();
 		
@@ -34,4 +39,27 @@ class QueueTest {
 		assertEquals(ql.getCurrent(), list.get(2));
 	}
 
+	@Test
+	void checkOrdering() {
+		ql = new QueueImpl();
+		final Map<PlayerImpl,Integer> mapResult = new HashMap<>();
+		final List<PlayerImpl> listExpected = new ArrayList<>();
+		final PlayerImpl p = new PlayerImpl("Ciao", PlayerColor.PINK);
+		final PlayerImpl p1 = new PlayerImpl("Ciao2", PlayerColor.PINK);
+		final PlayerImpl p2 = new PlayerImpl("Ciao4", PlayerColor.PINK);
+		
+		mapResult.put(p, 4);
+		mapResult.put(p1, 5);
+		mapResult.put(p2, 3);
+		ql.orderPlayerQueue(mapResult);
+		
+		listExpected.add(p1);
+		listExpected.add(p);
+		listExpected.add(p2);
+		System.out.println(listExpected);
+		System.out.println(ql.getStartingQueue());
+		assertEquals(listExpected, ql.getStartingQueue());
+		
+		
+	}
 }
