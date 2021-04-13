aa3aa0fffbdf9879e98fce4bf595b51f32ee9d2a
Endri Domi <endri.domi@studio.unibo.it>
2021-04-13 13:01:26 +0200

    implements method

--- src/main/java/application/minigame/memory/SecretCodeImpl.java
+++ src/main/java/application/minigame/memory/SecretCodeImpl.java
@@ -1,12 +1,18 @@
 package application.minigame.memory;
 
+import java.util.Random;
+
 public class SecretCodeImpl implements SecretCode{
 
+    private long secretCode;
 
+    private Random r = new Random();
 
     @Override
     public Long generateSecretCode() {
-        return null;
+        secretCode = r.nextInt(6);
+        System.out.println(secretCode);
+        return secretCode;
     }
 
     @Override
