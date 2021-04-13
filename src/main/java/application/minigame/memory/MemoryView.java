a4f233a3c0aba576d385c66eb8cf22c67a9f7f4b
Endri Domi <endri.domi@studio.unibo.it>
2021-04-13 12:34:09 +0200

    Update MemoryView.java

--- src/main/java/application/minigame/memory/MemoryView.java
+++ src/main/java/application/minigame/memory/MemoryView.java
@@ -1,4 +1,40 @@
 package application.minigame.memory;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MemoryView {
+
+    private static final String LAYOUT_LOCATION = "layouts/rps.fxml";
+    private static final String LOGO_LOCATION = "logo.png";
+    private final Stage primaryStage = new Stage();
+
+    public MemoryView() {
+        try {
+            System.out.println(ClassLoader.getSystemResource(LAYOUT_LOCATION));
+
+            final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource(LAYOUT_LOCATION));
+            loader.setController(this);
+            final Scene scene = new Scene(loader.load());
+            primaryStage.initModality(Modality.APPLICATION_MODAL);
+            primaryStage.setTitle("[GooseGame] Memory");
+            primaryStage.getIcons().add(new Image(LOGO_LOCATION));
+            primaryStage.setOnHiding(e -> primaryStage.setIconified(true));
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+
+
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void show() {
+        primaryStage.showAndWait();
+    }
 }
