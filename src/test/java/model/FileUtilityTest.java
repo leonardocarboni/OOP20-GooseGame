079613f51a8c214adfde829381cbc81c42e0c866
Andrea Ongaro <andrea.ongaro2@studio.unibo.it>
2021-04-15 20:27:25 +0200

    Update tests

--- src/test/java/model/FileUtilityTest.java
+++ src/test/java/model/FileUtilityTest.java
@@ -1,4 +1,5 @@
 package model;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -15,46 +16,46 @@ import utility.file.FileUtilityImpl;
 
 class FileUtilityTest {
 
-	@Test
-	public void writeReadFromFiles() {
-		final FileUtilityImpl<PlayerImpl> c = new FileUtilityImpl<>("GooseRanking.json");
-		final List<PlayerImpl> list = new ArrayList<>();
-		list.add(new PlayerImpl("Name1", PlayerColor.PINK));
-		list.add(new PlayerImpl("Name2", PlayerColor.PINK));
-		list.add(new PlayerImpl("Name3", PlayerColor.PINK));
-		c.saveInformation(list);
+    @Test
+    public void writeReadFromFiles() {
+        final FileUtilityImpl<PlayerImpl> c = new FileUtilityImpl<>("GooseRanking.json");
+        final List<PlayerImpl> list = new ArrayList<>();
+        list.add(new PlayerImpl("Name1", PlayerColor.PINK));
+        list.add(new PlayerImpl("Name2", PlayerColor.PINK));
+        list.add(new PlayerImpl("Name3", PlayerColor.PINK));
+        c.saveInformation(list);
 
-		List<PlayerImpl> fromFile = new ArrayList<>();
-		try {
-			fromFile = c.loadInformation(PlayerImpl.class);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		assertSame(fromFile.size(),list.size());
-		assertEquals(fromFile,list);
-	}
-	
-	@Test
-	public void writeReadFromFile() {
-		final FileUtilityImpl<String> c2 = new FileUtilityImpl<>("GooseRanking.json");
-		final List<String> list2 = new ArrayList<>();
-		list2.add("Name1");
-		list2.add("Name2");
-		c2.saveInformation(list2);
+        List<PlayerImpl> fromFile = new ArrayList<>();
+        try {
+            fromFile = c.loadInformation(PlayerImpl.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        assertSame(fromFile.size(), list.size());
+        assertEquals(fromFile, list);
+    }
 
-		List<String> fromFile2 = new ArrayList<>();
-		try {
-			fromFile2 = c2.loadInformation(String.class);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		assertEquals(fromFile2,list2);
-	}
+    @Test
+    public void writeReadFromFile() {
+        final FileUtilityImpl<String> c2 = new FileUtilityImpl<>("GooseRanking.json");
+        final List<String> list2 = new ArrayList<>();
+        list2.add("Name1");
+        list2.add("Name2");
+        c2.saveInformation(list2);
 
-	@Test
-	public void checkException() {
-		final FileUtilityImpl<String> c2 = new FileUtilityImpl<>("NotFound.json");
-		assertThrows(FileNotFoundException.class,() -> c2.loadInformation(String.class));
-	}
+        List<String> fromFile2 = new ArrayList<>();
+        try {
+            fromFile2 = c2.loadInformation(String.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(fromFile2, list2);
+    }
+
+    @Test
+    public void checkException() {
+        final FileUtilityImpl<String> c2 = new FileUtilityImpl<>("NotFound.json");
+        assertThrows(FileNotFoundException.class, () -> c2.loadInformation(String.class));
+    }
 }
--- src/test/java/model/QueueTest.java
+++ src/test/java/model/QueueTest.java
@@ -1,4 +1,5 @@
 package model;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
@@ -6,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import model.player.PlayerColor;
@@ -15,50 +15,49 @@ import model.queue.QueueImpl;
 
 class QueueTest {
 
-	private QueueImpl ql;
+    private QueueImpl ql;
 
-	@Test
-	void iterate() {
-		ql = new QueueImpl();
-		final List<PlayerImpl> list = new ArrayList<>();
-		list.add(new PlayerImpl("Ciao", PlayerColor.PINK));
-		list.add(new PlayerImpl("Ciao2", PlayerColor.PINK));
-		list.add(new PlayerImpl("Ciao4", PlayerColor.PINK));
-		ql.setStartingQueue(list);
-		ql.resetIterator();
-		
-		assertEquals(ql.getCurrent(), list.get(0));
-		assertEquals(ql.next(), list.get(1));
-		assertEquals(ql.next(), list.get(2));
-		
-		for(int i = 0; i < 9;i++) {
-			ql.next();
-		}
-		
-		assertEquals(ql.getCurrent(), list.get(2));
-	}
+    @Test
+    void iterate() {
+        ql = new QueueImpl();
+        final List<PlayerImpl> list = new ArrayList<>();
+        list.add(new PlayerImpl("Ciao", PlayerColor.PINK));
+        list.add(new PlayerImpl("Ciao2", PlayerColor.PINK));
+        list.add(new PlayerImpl("Ciao4", PlayerColor.PINK));
+        ql.setStartingQueue(list);
+        ql.resetIterator();
 
-	@Test
-	void checkOrdering() {
-		ql = new QueueImpl();
-		final Map<PlayerImpl,Integer> mapResult = new HashMap<>();
-		final List<PlayerImpl> listExpected = new ArrayList<>();
-		final PlayerImpl p = new PlayerImpl("Ciao", PlayerColor.PINK);
-		final PlayerImpl p1 = new PlayerImpl("Ciao2", PlayerColor.PINK);
-		final PlayerImpl p2 = new PlayerImpl("Ciao4", PlayerColor.PINK);
-		
-		mapResult.put(p, 4);
-		mapResult.put(p1, 5);
-		mapResult.put(p2, 3);
-		ql.orderPlayerQueue(mapResult);
-		
-		listExpected.add(p1);
-		listExpected.add(p);
-		listExpected.add(p2);
-		System.out.println(listExpected);
-		System.out.println(ql.getStartingQueue());
-		assertEquals(listExpected, ql.getStartingQueue());
-		
-		
-	}
+        assertEquals(ql.getCurrent(), list.get(0));
+        assertEquals(ql.next(), list.get(1));
+        assertEquals(ql.next(), list.get(2));
+
+        for (int i = 0; i < 3; i++) {
+            ql.next();
+        }
+
+        assertEquals(ql.getCurrent(), list.get(2));
+    }
+
+    @Test
+    void checkOrdering() {
+        ql = new QueueImpl();
+        final Map<PlayerImpl, Integer> mapResult = new HashMap<>();
+        final List<PlayerImpl> listExpected = new ArrayList<>();
+        final PlayerImpl p = new PlayerImpl("Ciao", PlayerColor.PINK);
+        final PlayerImpl p1 = new PlayerImpl("Ciao2", PlayerColor.PINK);
+        final PlayerImpl p2 = new PlayerImpl("Ciao4", PlayerColor.PINK);
+
+        mapResult.put(p, 4);
+        mapResult.put(p1, 0);
+        mapResult.put(p2, 3);
+        ql.orderPlayerQueue(mapResult);
+
+        listExpected.add(p);
+        listExpected.add(p2);
+        listExpected.add(p1);
+        System.out.println(listExpected);
+        System.out.println(ql.getStartingQueue());
+        assertEquals(listExpected, ql.getStartingQueue());
+
+    }
 }
