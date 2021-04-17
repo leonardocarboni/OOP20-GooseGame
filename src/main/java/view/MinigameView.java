9bf8ec1279201c219e18af0cf627bd188c751215
Leonardo Carboni <carboapple@gmail.com>
2021-04-17 18:59:52 +0200

    Update minigame view for quit button support

--- src/main/java/view/MinigameView.java
+++ src/main/java/view/MinigameView.java
@@ -1,13 +1,26 @@
 package view;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MinigameView extends View {
+
+    @FXML
+    private Button quitButton;
 
     public MinigameView(final ViewType gameType) {
         super.createStage(gameType);
-        this.getStage().setOnCloseRequest(e -> e.consume());
+        this.getStage().setOnCloseRequest(Event::consume);
+        quitButton.setDisable(true);
+        quitButton.setOnAction(e -> this.close());
     }
 
     public void showAndWaitResult() {
         this.getStage().showAndWait();
+    }
+
+    public void enableQuitButton(){
+        this.quitButton.setDisable(false);
     }
 }
