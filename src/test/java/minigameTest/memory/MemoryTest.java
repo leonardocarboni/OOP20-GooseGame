9adb90d0bb5ad3ebf05fe074192e061033163552
Endri Domi <endri.domi@studio.unibo.it>
2021-04-21 19:46:31 +0200

    Update MemoryTest.java

--- src/test/java/minigameTest/memory/MemoryTest.java
+++ src/test/java/minigameTest/memory/MemoryTest.java
@@ -4,9 +4,7 @@ import model.secretcode.SecretCode;
 import model.secretcode.SecretCodeImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
