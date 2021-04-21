7222782a3fde8b2c7e54cb1fd227f1eb2f3a1044
Endri Domi <endri.domi@studio.unibo.it>
2021-04-21 19:46:52 +0200

    add method for testing

--- src/main/java/model/choice/RockPaperScissorsChoice.java
+++ src/main/java/model/choice/RockPaperScissorsChoice.java
@@ -1,5 +1,6 @@
 package model.choice;
 
+import java.util.List;
 import java.util.Random;
 
 
@@ -26,5 +27,23 @@ public enum RockPaperScissorsChoice {
     public static RockPaperScissorsChoice getRandomChoice() {
         final Random r = new Random();
         return RockPaperScissorsChoice.values()[r.nextInt(RockPaperScissorsChoice.values().length)];
+    }
+
+    /**
+     * Is use for testing.
+     * Creates a list of hypothetical computer choices
+     * @return list of choice.
+     */
+    public List<RockPaperScissorsChoice> setComputerChoice() {
+        return List.of(PAPER, SCISSORS, ROCK);
+    }
+
+    /**
+     * Is use for testing.
+     * Creates a list of hypothetical user choices
+     * @return list of choice;
+     */
+    public List<RockPaperScissorsChoice> setInputChoice() {
+        return List.of(ROCK, PAPER, SCISSORS);
     }
 }
