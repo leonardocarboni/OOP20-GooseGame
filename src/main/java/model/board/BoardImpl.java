ee4ecc38aff573e53f3e8c3853955e78e464aaaa
davidedenicolo99 <davide.denicolo@studio.unibo.it>
2021-04-22 15:21:04 +0200

    bug fixed on Board

--- src/main/java/model/board/BoardImpl.java
+++ src/main/java/model/board/BoardImpl.java
@@ -27,7 +27,7 @@ public class BoardImpl implements Board {
         for (int i = 1; i < size; i++) {
             if (i % MINIGAME_INTERVAL == 0) {
                minigameNumber = minigameNumber == minigames.size() ? 0 : minigameNumber;
-               boxes.add(minigames.get(0));
+               boxes.add(minigames.get(minigameNumber));
                minigameNumber++;
             } else {
                 boxes.add(Box.NORMAL);
