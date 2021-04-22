6725bad693bfc3deba17f2b803c0830f8992b32e
Leonardo Carboni <carboapple@gmail.com>
2021-04-23 00:38:17 +0200

    Fix jar file bug for phrasecatch

--- src/main/java/model/phrase/PhraseImpl.java
+++ src/main/java/model/phrase/PhraseImpl.java
@@ -1,25 +1,30 @@
 package model.phrase;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class PhraseImpl implements Phrase {
 
-    private static final String PHRASES_FILENAME = "src/main/resources/sentences.txt";
+    private static final String PHRASES_FILENAME = "phraseCatch/sentences.txt";
     private String phrase;
 
     public String generatePhrase() {
-        final File file = new File(PHRASES_FILENAME);
-        try (RandomAccessFile f = new RandomAccessFile(file, "r")) {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PHRASES_FILENAME);
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             final Random random = new Random();
-            final long randomLocation = random.nextInt((int) f.length());
-            f.seek(randomLocation);
-            f.readLine();
-            phrase = f.readLine();
-        } catch (final IOException ex) {
-            System.out.println("Couldn't load file");
+            List<String> lines = reader.lines().collect(Collectors.toList());
+            phrase = lines.get(random.nextInt(lines.size()));
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return phrase;
     }
