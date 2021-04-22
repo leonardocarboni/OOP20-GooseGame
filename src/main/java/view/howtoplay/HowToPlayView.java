e7c71b736423fece348e4505e6072daf08dd4a76
Leonardo Carboni <carboapple@gmail.com>
2021-04-22 12:42:35 +0200

    remove warning in howtoplayview

--- src/main/java/view/howtoplay/HowToPlayView.java
+++ src/main/java/view/howtoplay/HowToPlayView.java
@@ -8,13 +8,13 @@ import view.ViewType;
 
 public class HowToPlayView extends NormalView {
 
-    private static final String MAINGAMEDESC = "Start by choosing the names of the players and their color,\n" +
-            "remember that you can not play with less than two people and players can not have the same name!\n" +
-            "The game starts with an initial phase in which all players must roll the dice,\n" +
-            "the starting order is based on the score you make.\n" +
-            "To advance, just roll the dice until you reach the final square.\n" +
-            "In the path there are various mini-games that can help you to advance or turn back.\n" +
-            "If by rolling the dice you overtake the final square, you will go back by the number you overtook it.\n";
+    private static final String MAINGAMEDESC = "Start by choosing the names of the players and their color,\n"
+            + "remember that you can not play with less than two people and players can not have the same name!\n"
+            + "The game starts with an initial phase in which all players must roll the dice,\n"
+            + "the starting order is based on the score you make.\n"
+            + "To advance, just roll the dice until you reach the final square.\n"
+            + "In the path there are various mini-games that can help you to advance or turn back.\n"
+            + "If by rolling the dice you overtake the final square, you will go back by the number you overtook it.\n";
 
     @FXML
     private Label howToPlayDescLabel, cableConnectDescLabel, ticTacToeDescLabel, phraseCatchDescLabel,
