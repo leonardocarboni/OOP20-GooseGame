0d5e1eb2d617a37b8a0ceba1a7278b1d6ede2773
Endri Domi <endri.domi@studio.unibo.it>
2021-04-24 11:37:49 +0200

    remove test

--- src/test/java/minigame/memory/MemoryTest.java
+++ src/test/java/minigame/memory/MemoryTest.java
@@ -14,6 +14,11 @@ public class MemoryTest {
 
     private static final int SIZE = 5;
     private static final int EXPECTED_ERRORS = 5;
+    private static final int NUM_1 = 1;
+    private static final int NUM_2 = 2;
+    private static final int NUM_3 = 3;
+    private static final int NUM_4 = 4;
+    private static final int NUM_5 = 5;
     private static SecretCode secretCode;
 
     @BeforeAll
@@ -22,19 +27,10 @@ public class MemoryTest {
     }
 
     @Test
-    public void checkSize() {
-        secretCode.generateSecretCode();
-        final var sc = secretCode.getCode();
-        final var size = sc.length();
-
-        assertEquals(SIZE, size);
-    }
-
-    @Test
     public void checkError() {
         var errors = 0;
-        var inputCode = List.of(7, 4, 3, 2, 1);
-        secretCode.setCode(List.of(1, 2, 3, 4, 5));
+        final var inputCode = List.of(NUM_5, NUM_4, NUM_3, NUM_2, NUM_1);
+        secretCode.setCode(List.of(NUM_1, NUM_3, NUM_2, NUM_5, NUM_4));
         final var sc = secretCode.getCode();
 
         final List<Integer> code = sc.chars()
